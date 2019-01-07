98c9a729312faedc2ef2157fdeb165b5f31d441a
KimSickal <shihwan1004@naver.com>
2019-01-07 12:56:50 +0900

    ts붙임

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+import './App.css';
+
+export class App extends React.Component {
+	render() {
+		return (
+			<div className="App">
+				{'asdf'}
+			</div>
+		);
+	}
+}
