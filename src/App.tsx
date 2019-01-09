1099c26f71dbd68b2f17682f2c3b33ea5760f738
KimSickal <shihwan1004@naver.com>
2019-01-09 18:08:50 +0900

    네개로 불림

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,68 @@ import {
 	GridContainer,
 } from './containers';
 
+import {
+	createStore,
+	applyMiddleware,
+} from 'redux';
+
+import {
+	Provider,
+} from 'react-redux';
+
+import thunk from 'redux-thunk';
+
+import {
+	reducers,
+} from './reducers';
+
 import './App.css';
 
 export class App extends React.Component {
 	public render() {
 		return (
-			<div className="App">
-				<KeyboardHandlerContainer/>
-				<GridContainer/>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'row',
+				}}
+			>
+				<div>
+					<Provider
+						store={createStore(reducers, applyMiddleware(thunk))}
+					>
+						<div className="App">
+							<KeyboardHandlerContainer/>
+							<GridContainer/>
+						</div>
+					</Provider>
+					<Provider
+						store={createStore(reducers, applyMiddleware(thunk))}
+					>
+						<div className="App">
+							<KeyboardHandlerContainer/>
+							<GridContainer/>
+						</div>
+					</Provider>
+				</div>
+				<div>
+					<Provider
+						store={createStore(reducers, applyMiddleware(thunk))}
+					>
+						<div className="App">
+							<KeyboardHandlerContainer/>
+							<GridContainer/>
+						</div>
+					</Provider>
+					<Provider
+						store={createStore(reducers, applyMiddleware(thunk))}
+					>
+						<div className="App">
+							<KeyboardHandlerContainer/>
+							<GridContainer/>
+						</div>
+					</Provider>
+				</div>
 			</div>
 		);
 	}
--- src/index.tsx
+++ src/index.tsx
@@ -2,32 +2,13 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import {
-	createStore,
-	applyMiddleware,
-} from 'redux';
-
-import {
-	Provider,
-} from 'react-redux';
-
-import thunk from 'redux-thunk';
-
 import './index.css';
-
-import {
-	reducers,
-} from './reducers';
 
 import {
 	App,
 } from './App';
 
 ReactDOM.render(
-	<Provider
-		store={createStore(reducers, applyMiddleware(thunk))}
-	>
-		<App />
-	</Provider>,
+	<App />,
 	document.getElementById('root'),
 );
