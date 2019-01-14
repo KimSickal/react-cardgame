5218e27b8910cc1320f9a97e2924344d54e029ea
KimSickal <shihwan1004@naver.com>
2019-01-14 16:37:51 +0900

    Updates

---
+++ precache-manifest.b167e108accc0edbb8b5242fee2fb7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4a55a9cf0cf227263c5",
+    "url": "/react-cardgame/static/js/runtime~main.f4a55a9c.js"
+  },
+  {
+    "revision": "2a242261028549c664e2",
+    "url": "/react-cardgame/static/js/main.2a242261.chunk.js"
+  },
+  {
+    "revision": "fdbb621fec9c65df3aa3",
+    "url": "/react-cardgame/static/js/1.fdbb621f.chunk.js"
+  },
+  {
+    "revision": "2a242261028549c664e2",
+    "url": "/react-cardgame/static/css/main.50b67257.chunk.css"
+  },
+  {
+    "revision": "d9d9f4204c9ef4244c31096427fb0abc",
+    "url": "/react-cardgame/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-snake-game/precache-manifest.a5ec068359dcebee2187f3dccfc87411.js"
+  "/react-cardgame/precache-manifest.b167e108accc0edbb8b5242fee2fb7c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-snake-game/index.html", {
+workbox.routing.registerNavigationRoute("/react-cardgame/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
