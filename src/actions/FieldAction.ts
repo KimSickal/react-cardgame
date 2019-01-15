fd5f7bc753e12a7fa37259be3a75baf7a124f41f
KimSickal <shihwan1004@naver.com>
2019-01-15 12:09:01 +0900

    캐릭터 추가하기 구현

--- src/actions/FieldAction.ts
+++ src/actions/FieldAction.ts
@@ -6,19 +6,26 @@ import {
 	SetCellAction,
 	FieldKeys,
 	MoveCharacterAction,
+	AppendCharacterAction,
 } from './types';
 
 import {
 	position,
 	CellType,
 	Direction,
+	Character,
+	CellCode,
 } from '../models';
 
 import {
 	State,
 } from '../reducers';
 
-export function setCell(targetPosition: position, cellType: CellType): SetCellAction {
+import {
+	getField,
+} from '../selectors';
+
+function setCell(targetPosition: position, cellType: CellType): SetCellAction {
 	return {
 		type: FieldKeys.SET_CELL,
 		targetPosition: targetPosition,
@@ -37,5 +44,29 @@ export function moveCharacter(targetPosition: position, characterIndex: number):
 export function moveCharacterIfCould(direction: Direction, characterIndex: number) {
 	return (dispatch: Dispatch<any>, getState: () => State) => {
 		console.log('asdf');
+	};
+}
+
+function appendCharacter(targetCharacter: Character): AppendCharacterAction {
+	return {
+		type: FieldKeys.APPEND_CHARACTER,
+		targetCharacter: targetCharacter,
+	};
+}
+
+function couldAppendCharacter(state: State, targetCharacter: Character) {
+	if(getField(state)[targetCharacter.position.posX][targetCharacter.position.posY] === CellCode.CELL_BLANK) {
+		return true;
+	}
+	return false;
+}
+
+export function appendCharacterIfCould(targetCharacter: Character) {
+	return (dispatch: Dispatch<any>, getState: () => State) => {
+		const state = getState();
+		if(couldAppendCharacter(state, targetCharacter)) {
+			dispatch(appendCharacter(targetCharacter));
+			dispatch(setCell(targetCharacter.position, targetCharacter));
+		}
 	};
 }
--- src/actions/types/FieldActionTypes.ts
+++ src/actions/types/FieldActionTypes.ts
@@ -5,11 +5,13 @@ import {
 import {
 	position,
 	CellType,
+	Character,
 } from '../../models';
 
 export enum FieldKeys {
 	SET_CELL = 'SET_CELL',
 	MOVE_CHARACTER = 'MOVE_CHARACTER',
+	APPEND_CHARACTER = 'APPEND_CHARACTER',
 }
 
 export interface SetCellAction extends Action {
@@ -24,7 +26,13 @@ export interface MoveCharacterAction extends Action {
 	characterIndex: number;
 }
 
+export interface AppendCharacterAction extends Action {
+	type: FieldKeys.APPEND_CHARACTER;
+	targetCharacter: Character;
+}
+
 export type FieldAction = (
 	| SetCellAction
 	| MoveCharacterAction
+	| AppendCharacterAction
 );
